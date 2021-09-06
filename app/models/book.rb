e53e8a35a18b37cdaa80e4ff7f890c0bed9175a3
seitosugino <0jb4f61k3853v2f@gmail.com>
2021-09-06 09:47:19 +0000

    7b

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,7 +6,10 @@ class Book < ApplicationRecord
 	has_many :book_comments, dependent: :destroy
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
-  
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } 
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } 
+  scope :created_thisweek, -> { where(created_at: 1.week.ago.beginning_of_day..Time.zone.now.end_of_day) } 
+  scope :created_lastweek, -> { where(created_at: 1.week.ago.all_day) } 
 
 	def favorited_by?(user)
 		favorites.where(user_id: user.id).exists?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_05_124205) do
+ActiveRecord::Schema.define(version: 2021_09_05_133425) do
 
   create_table "book_comments", force: :cascade do |t|
     t.integer "user_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_09_05_124205) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "impressions_count", default: 0
+    t.float "rate"
   end
 
   create_table "entries", force: :cascade do |t|
